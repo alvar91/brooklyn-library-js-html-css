a01727a86172913b24124dc7ffb801cab359fae6
alvar91 <aleksey91scorp@bk.ru>
2023-08-03 21:11:15 +0300

    feat: burger script

--- js/main.js
+++ js/main.js
@@ -1,31 +1,75 @@
-(function () {
-  const duration = 3000;
+document.addEventListener("DOMContentLoaded", function () {
+  // Burger menu
+  (function () {
+    const burger = document.getElementById("burger-menu");
+    const list = document.querySelector(".nav__list");
 
-  function initSlider() {
-    const swiperPromo = new Swiper(".js-about-swiper", {
-      spaceBetween: 25,
-      resizeObserver: true,
-      updateOnWindowResize: true,
-      direction: "horizontal",
-      slidesPerGroup: 1,
-      slidesPerView: 3,
+    if(!burger || !list) return;
 
-      pagination: {
-        el: ".swiper-pagination",
-        type: "bullets",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return `<button class="${className} js-about-promo">
+    function closeMenu() {
+      burger.classList.remove("js-burger-open");
+      list.classList.remove("show-mobile-menu");
+    }
+
+    function openMenu() {
+      burger.classList.add("js-burger-open");
+      list.classList.add("show-mobile-menu");
+    }
+
+    document.body.addEventListener("click", (e) => {
+      const target = e.target;
+      if (
+        !target.closest(".burger-menu") &&
+        !target.classList.contains("show-mobile-menu")
+      ) {
+        closeMenu();
+      }
+    });
+
+    burger.addEventListener("click", () => {
+      if (burger.classList.contains("js-burger-open")) {
+        closeMenu();
+        return;
+      }
+
+      openMenu();
+    });
+
+    window.addEventListener("resize", () => {
+      closeMenu();
+    });
+  })();
+
+  // Swiper slider
+  (function () {
+    const duration = 3000;
+
+    function initSlider() {
+      const swiperPromo = new Swiper(".js-about-swiper", {
+        spaceBetween: 25,
+        resizeObserver: true,
+        updateOnWindowResize: true,
+        direction: "horizontal",
+        slidesPerGroup: 1,
+        slidesPerView: 3,
+
+        pagination: {
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true,
+          renderBullet: function (index, className) {
+            return `<button class="${className} js-about-promo">
             <div class="bullet"></div>
           </button>`;
+          },
         },
-      },
 
-      //   autoplay: {
-      //     delay: duration,
-      //   },
-    });
-  }
+        //   autoplay: {
+        //     delay: duration,
+        //   },
+      });
+    }
 
-  initSlider();
-})();
+    initSlider();
+  })();
+});
