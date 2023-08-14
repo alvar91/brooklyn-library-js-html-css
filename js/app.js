ba996a34971da4a3dffe397b1065cf91c45e1739
alvar91 <aleksey91scorp@bk.ru>
2023-08-14 21:49:27 +0300

    feat: app

---
+++ js/app.js
@@ -0,0 +1,24 @@
+import { Burger } from "./components/burger.js";
+import { Slider } from "./components/slider.js";
+
+export default class App {
+  init() {
+    const burger = new Burger({
+      burgerSelector: ".js-burger-menu",
+      burgerOpenSelector: ".js-burger-open",
+      showMobileMenuSelector: ".show-mobile-menu",
+      listSelector: ".js-nav__list",
+    });
+    burger.init();
+
+    const slider = new Slider({
+      sliderSelector: ".js-slider",
+      sliderContainerSelector: ".js-slider-container",
+      slidesSelector: ".js-slide",
+      buttonsContainerSelector: ".js-slide-buttons",
+      prevSlideButtonSelector: ".js-prev-arrow",
+      nextSlideButtonSelector: ".js-next-arrow",
+    });
+    slider.init();
+  }
+}
