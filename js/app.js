702ad975680ea25d3160856df33d3460561f5a66
alvar91 <aleksey91scorp@bk.ru>
2023-08-15 16:50:05 +0300

    feat: app

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 import { Burger } from "./components/burger.js";
 import { Slider } from "./components/slider.js";
+import { Modal } from "./components/modal.js";
 
 export default class App {
   init() {
@@ -20,5 +21,41 @@ export default class App {
       nextSlideButtonSelector: ".js-next-arrow",
     });
     slider.init();
+
+    const loginModal = new Modal({
+      modalSelector: ".js-login-modal",
+      openModalSelector: ".js-open-login",
+      closeModalSelector: ".js-close-login",
+      activeClassSelector: ".active",
+      contentSelector: ".js-login-modal-content",
+    });
+    loginModal.init();
+
+    const signUpModal = new Modal({
+      modalSelector: ".js-sign-up-modal",
+      openModalSelector: ".js-open-sign-up",
+      closeModalSelector: ".js-close-sign-up",
+      activeClassSelector: ".active",
+      contentSelector: ".js-sign-up-modal-content",
+    });
+    signUpModal.init();
+
+    const profileModal = new Modal({
+      modalSelector: ".js-profile-modal",
+      openModalSelector: ".js-open-profile",
+      closeModalSelector: ".js-close-profile",
+      activeClassSelector: ".active",
+      contentSelector: ".js-profile-modal-content",
+    });
+    profileModal.init();
+
+    const libraryCardModal = new Modal({
+      modalSelector: ".js-library-card-modal",
+      openModalSelector: ".js-open-library-card",
+      closeModalSelector: ".js-close-library-card",
+      activeClassSelector: ".active",
+      contentSelector: ".js-library-card-modal-content",
+    });
+    libraryCardModal.init();
   }
 }
