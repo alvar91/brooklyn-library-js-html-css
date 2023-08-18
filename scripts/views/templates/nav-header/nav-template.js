7c18a80b21039f8f097951cd9ee08a7cf5ab979e
alvar91 <aleksey91scorp@bk.ru>
2023-08-18 11:54:30 +0300

    feat: header-nav

---
+++ scripts/views/templates/nav-header/nav-template.js
@@ -0,0 +1,19 @@
+const createNavMenuTemplate = ({ route, title }) => {
+  return `
+        <li class="nav__item">
+            <a class="link" href="${route}">${title}</a>
+        </li>`;
+};
+
+export const NavTemplate = (navList) => {
+  return `
+    <nav class="header__nav nav">
+        <ul class="nav__list js-nav__list">
+        ${navList
+          .map((item) => {
+            return createNavMenuTemplate(item);
+          })
+          .join("")}
+        </ul>
+    </nav>`;
+};
