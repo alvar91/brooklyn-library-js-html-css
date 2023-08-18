bedaf98395033401b5c2228188541038c75b18bd
alvar91 <aleksey91scorp@bk.ru>
2023-08-18 11:54:03 +0300

    feat: burger

---
+++ scripts/views/templates/burger/burger-template.js
@@ -0,0 +1,7 @@
+export const BurgerTemplate = () => {
+  return `<button class="burger-menu js-burger-menu" aria-label="Развернуть меню навигации">
+      <span class="burger-menu__first-line"></span>
+      <span class="burger-menu__second-line"></span>
+      <span class="burger-menu__third-line"></span>
+    </button>`;
+};
